a283db06e94a529088b38e44507743f0b84bed36
Lia <liavramirez21@gmail.com>
2018-11-01 12:15:41 -0500

    jsbuttons

---
+++ public/js/buttons.js
@@ -0,0 +1,49 @@
+
+
+$("button1").on("click", function() {
+    $("container").empty();
+   
+    $( "container" ).append( "newDiv" );
+
+    // timer js -- setInterval(function time(){
+  //var d = new Date();
+ // var hours = 24 - d.getHours();
+ // var min = 60 - d.getMinutes();
+ // if((min + '').length == 1){
+ //   min = '0' + min;
+ // }
+  //var sec = 60 - d.getSeconds();
+  //if((sec + '').length == 1){
+        //sec = '0' + sec;
+ // }
+  //jQuery('#the-final-countdown p').html(hours+':'+min+':'+sec)
+//}, 1000);
+//when clicked log the time when buttton clicked
+//when clicked array.push(item1, item2, ..., itemX)-- pushing data to an array
+
+    
+});
+
+$("button2").on("click", function() {
+    $("container").empty();
+    
+    $( "container" ).append( "newDiv" );
+   
+   
+      });
+
+//--- show that pushed data into a table that will be appended after click
+
+  $("button3").on("click", function() {
+        $("container").empty();
+        $( "container" ).append( "newDiv" );
+
+       
+       });
+     //THIS IS THE TIME OFF REQUEST TEXT BOX
+    // <form>
+    // <div class="form-group">
+    //<label for="exampleFormControlTextarea1">Example textarea</label>
+    //<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  //</div>
+//</form>
