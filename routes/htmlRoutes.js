8ac629554571faee776d6edc97afa87ff2f6ffd0
nfgrawker <nfgrawker@gmail.com>
2018-11-01 11:06:31 -0500

    Added html routes as well as fixes to test env

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,22 +3,19 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/employee", function(req, res) {
+    res.sendFile(path.join(__dirname, '../public', 'employee.html'));
   });
+
+  app.get("/manager", function(req, res) {
+    res.sendFile(path.join(__dirname, '../public', 'manager.html'));
+  });
+
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
