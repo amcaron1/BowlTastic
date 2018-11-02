7ea088ab418dc2f24717e0d3ee0aa67d0cb53e7b
nfgrawker <nfgrawker@gmail.com>
2018-11-02 12:19:37 -0500

    All hours routes completed

--- routes/hours-api-paths.js
+++ routes/hours-api-paths.js
@@ -1,30 +1,28 @@
 var db = require("../models");
+var Sequelize = require('sequelize');
 
 module.exports = function(app) {
   app.get("/api/hours", function(req, res) {
-    db.Hour.findAll({}).then(function(dbHours) {
+    db.Hour.findAll({where:{EmployeeId:req.user.id}}).then(function(dbHours) {
       res.json(dbHours);
     });
   });
 
-  app.get("/api/hours/in/:id", function(req,res) {
-    console.log("this is the user:",req.user)
-      db.Hours.findAll({where: {id: req.params.id}}).then(function(dbHours) {
-          res.json(dbHours);
-      })
-  })
-
-  app.get("/api/hours/out/:id", function(req,res) {
-    db.Hours.findAll({where: {id: req.params.id}}).then(function(dbHours) {
-        res.json(dbHours);
-        })
-    })
 
     app.post("/api/hours", function(req, res) {
-        db.Hours.create(req.body).then(function(dbHours) {
-          res.json(dbHours);
-        });
-      });
+      console.log(req.user)
+      db.Hour.findAll({limit:1,where:{EmployeeId:req.user.id},order:[ [ 'createdAt', 'DESC' ]]}).then(user=>{
+        console.log(user)
+        if(user[0].dataValues.timeout == null){
+          var currentid = user[0].dataValues.id
+          db.Hour.update({timeout:Sequelize.fn('NOW')},{where:{id:currentid}})
+        }
+        else{
+          db.Hour.create({timein:Sequelize.fn('NOW'),EmployeeId:req.user.id})
+        }
+      })
+      res.json("{clocked:'yep!'}")
+    });
 
     app.delete("/api/hours/delete/:id", function(req, res) {
         db.Hours.destroy({
