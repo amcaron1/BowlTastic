4c44291e6988a7be34a2ffdbec39be18b23cb409
Dylan Airhart <38706775+nfgrawker@users.noreply.github.com>
2018-11-06 11:17:02 -0600

    Update server.js

--- server.js
+++ server.js
@@ -47,16 +47,5 @@ db.sequelize.sync(syncOptions).then(function() {
     );
   });
 });
-<<<<<<< HEAD
 
-=======
-db.Employee.findAll({}).then(response=>{
-        for(let i in response){
-            var hash = bcrypt.hashSync(response[i].dataValues.password, 10);
-
-            db.Employee.update({password:hash},{where:{id:response[i].dataValues.id}})
-
-        }
-});
->>>>>>> d0c3c0a34b4fefe0cb4488637ad2072eee9d2d32
 module.exports = app;
